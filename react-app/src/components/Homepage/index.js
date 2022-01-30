b48c10432e554e2e0e7bc0d630dcf28caf7130ed
adeline <adelineswires@gmail.com>
2022-01-29 17:02:32 -0800

    small change to update remote file matching local

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -22,13 +22,27 @@ export default function HomePage(){
     //let sessionLinks;
     //if (sessionUser) {
         let sessionLinks = (<>
-        <div className='main-splash-container'>
-                <div className="homePage">
-                <img className="mapFront" title="Artist: Michael Tompsett" src="https://user-images.githubusercontent.com/86431563/150725585-c384a920-246a-4647-9394-cf414b291875.jpg"></img>
+            <div className='main-splash-container'>
+                    <div className="homePage">
+                    <img className="mapFront" title="Artist: Michael Tompsett" src="https://user-images.githubusercontent.com/86431563/150725585-c384a920-246a-4647-9394-cf414b291875.jpg"></img>
+                    </div>
+                <div className='slogan-container'>
+                    <hr className='home-hr' size='8' />
+                    <div className='slogan'><i>See Some (S)miles</i></div>
+                    <hr className='home-hr' size='8' />
                 </div>
-
-        </div>
-        </>);
+                <div className='homepage-text-container'>
+                    <span className='text-info'>Map out and share with</span>
+                    <span className='text-info'>friends. Discover local</span>
+                    <span className='text-info'>events around you.</span>
+                    <NavLink to='/sign-up' className='home-sign-up'><b><i>Sign Up</i></b></NavLink>
+                    <div className='home-login-container'>
+                        <span className='login-q'>Already a member?</span>
+                        <NavLink to='/login' className='home-login'>LOG IN</NavLink>
+                    </div>
+                </div>
+            </div>
+            </>);
 
     // if (!isLoaded) {
     //     return (
@@ -41,7 +55,7 @@ export default function HomePage(){
 
     if (!isLoaded) {
         return (
-        <div id="t">
+        <div className="t4">
             <video id="mediaContent" preload="none" muted="" loop="" playsinline="" autoplay="" src="https://cdn.dribbble.com/users/4187655/screenshots/8421375/media/eaa37c1d3e989e818ab5cd3748a09867.mp4">
             </video>
             <div className="loadText">Loading</div>
@@ -51,7 +65,7 @@ export default function HomePage(){
 
     return (<>{isLoaded && (<>
         <div id="logBtns">{sessionLinks}</div>
-            <div id="t4">
+            <div className="t4">
                     <video id="mediaContent" preload="true" muted="" loop="" playsinline="" autoplay="" src="https://cdn.dribbble.com/users/4187655/screenshots/8421375/media/eaa37c1d3e989e818ab5cd3748a09867.mp4">
                     </video>
                     <div className="loadText">Loading</div>
